68e451e48c0c39bf047460d916033fa8512cdbee
benjamindelafuente01 <benjamindelafuentereyes01@gmail.com>
2024-09-03 15:18:22 -0600

    Script con las variables de conexion PDO

---
+++ Practica-Paginacion/config.php
@@ -0,0 +1,11 @@
+<?php
+
+    /*
+        Define los datos de tu conexion mediante PDO
+    */
+
+    define ('DATASOURCENAME', 'mysql:host=localhost; dbname=');
+    define ('USER', '');
+    define ('USERPASSWORD', '');
+
+?>
